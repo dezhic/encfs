0ffab9a0fa53c8a1b131ecc51d26e2bcfed337c6
dezhic <chen.dz@live.com>
2023-04-16 07:25:48 +0800

    size limit, update db

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '1024mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
