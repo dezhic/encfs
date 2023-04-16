cf5c567c2c869cb16fd7dbf3789727ee1e8944a2
dezhic <chen.dz@live.com>
2023-04-16 22:03:08 +0800

    update key size; update ui

--- browser/keygen.js
+++ browser/keygen.js
@@ -4,7 +4,7 @@ function generateKeyPair() {
     return window.crypto.subtle.generateKey(
         {
             name: "RSA-OAEP",
-            modulusLength: 4096,
+            modulusLength: 2048,
             publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
             hash: { name: "SHA-256" },
         },
